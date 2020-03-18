6c0bfff3fb1693af2a5674db671da6c8e445f664
unknown <matheus.sousa247@hotmail.com>
2020-03-17 23:27:53 -0300

    Reworking /contato

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -55,7 +55,7 @@
             </div>
 
             {{-- MAPA --}}
-            <div class="row my-5 px-5">
+            <div class="row my-5">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14629.55518769143!2d-46.6889560302246!3d-23.55447584999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m3!3e0!4m0!4m0!5e0!3m2!1spt-BR!2sbr!4v1575298739872!5m2!1spt-BR!2sbr" width="100%" height="350px" frameborder="0" style="border:1;" allowfullscreen=""></iframe>
             </div>
         </div>
