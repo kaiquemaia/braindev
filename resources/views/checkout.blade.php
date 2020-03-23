1866a680e4ccafd0084d7470f6e41ef3ac3c8c03
kaiquemaia <kaiquefeng1@gmail.com>
2020-03-23 00:13:46 -0300

    mask input telefone

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -57,7 +57,8 @@
 
                       </div>
                       <div class="form-group col-md-6">
-                        <input type="tel" class="form-control" id="telefone" name="telefone" placeholder="Digite seu Telefone *" required>
+                        <input type="tel" class="form-control" id="telefone" name="telefone" placeholder="Digite seu Telefone *" pattern="\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4,5}" required>
+                        <script type="text/javascript">$("#telefone").mask("(00) 0000-00009");</script>
                       </div>
                       <div class="form-group col-md-6">
                         <input type="text" class="form-control" id="endereco" name="endereço" placeholder="Seu endereço *" required>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,6 +20,9 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
 </head>
 <body>
 
